7f2901bb135fe8d31d3e1611333536486de9d81a
ajclarkin <mail@ajclarkin.co.uk>
2019-11-19 08:38:34 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ChargePoints
---
+++ models.py
@@ -0,0 +1,23 @@
+from flask_sqlalchemy import SQLAlchemy
+from app import app
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False    # added cause it keeos warning me
+db = SQLAlchemy(app)
+
+
+class ChargePoints(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+    point = db.Column(db.Integer)
+    connector = db.Column(db.Integer)
+    type = db.Column(db.String(30))
+
+    # This allows us to print the output of a query
+    def __repr__(self):
+        return f'{self.id} {self.name} {self.point} {self.connector} {self.type}'
+
+
+
+db.create_all()
